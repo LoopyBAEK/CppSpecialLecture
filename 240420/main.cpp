741380722537e5a26d59dbe4f11eba9f1f1fcdff
장미 <likipop@naver.com>
2024-04-20 13:11:10 +0900

    변수

--- 240420/main.cpp
+++ 240420/main.cpp
@@ -21,5 +21,50 @@ int main()
 
 	cout << "std namespace 사용" << endl;
 
+	/*
+	변수 : 값을 저장.
+	컴퓨터 용량의 최소 단위 : bit
+	1byte = 8bit
+	1kbyte = 1024byte
+	1mbyte = 1024kbyte
+	1gbyte = 1024mbyte
+	1tbyte = 1024gbyte
+
+	문자를 표현하는 방법 : ASCII 코드 표를 가지고 있다.
+	false : 0
+	true : 0이 아닌 모든 수
+	종류		|	용량		|	데이터	|	표현 범위			|	unsigned		|
+	char	|	1byte	|	문자		|	-128 ~ 127		|	0 ~ 255			|
+	bool	|	1byte	|	참/거짓	|	true / false	|	true / false	|
+	short	|	2byte	|	정수		|	-32768 ~ 32767	|	0 ~ 65535		|
+	int		|	4byte	|	정수		|	약 -22억 ~ 21억	|	0 ~ 약 43억		|
+	float	|	4byte	|	실수		|
+	double	|	8byte	|	실수		|
+	*/
+
+	// = : 대입 연산자. 오른쪽에 있는 값을 왼쪽의 변수에 대입한다.
+	int Number = 10;
+	Number = 20;
+
+	cout << Number << endl;
+
+	bool bTest = true;
+	bTest = false;
+
+	cout << bTest << endl;
+
+	// 영문이나 숫자는 1byte를 차지한다. 하지만 한글이나 한문 등의 문자들은 2byte를 차지하기 때문에 char 변수 하나에 저장하기 힘들다.
+	char cTest = 't';
+
+	cout << cTest << endl;
+
+	float fNumber = 3.14f;	// f를 붙이지 않으면 double 타입 실수가 됨.
+
+	cout << fNumber << endl;
+
+	double dNumber = 123.4567;
+
+	cout << dNumber << endl;
+
 	return 0;
 }
